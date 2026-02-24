8918d849da8c2586c4d344ca6a57ec5203922255
Shuy7wng <168526714+Shuy7wng@users.noreply.github.com>
2026-02-24 03:11:02 +0100

    added icon

--- includes/footer.php
+++ includes/footer.php
@@ -5,15 +5,42 @@
     </div>
     <div class="col footer-about">
       <h3>Chi Siamo</h3>
-      <p>Descrizione breve dell'azienda. Offriamo servizi innovativi e prodotti di qualità per i nostri clienti.</p>
+      <p>
+Nexus Space nasce dall’idea di connessione: un luogo digitale dove l’arte incontra il pubblico, 
+dove gli artisti emergenti trovano visibilità e dove ogni opera diventa occasione di dialogo, 
+scoperta e crescita condivisa.
+</p>
     </div>
     <div class="col footer-social">
       <h3>Seguici</h3>
       <ul class="social-icons">
-        <li><a href="#"><svg></svg></a></li>
-        <li><a href="#"><svg></svg></a></li>
-        <li><a href="#"><svg></svg></a></li>
-      </ul>
+  <li>
+    <a href="https://instagram.com" target="_blank">
+      <!-- Instagram -->
+      <svg viewBox="0 0 24 24" width="22" height="22" fill="currentColor">
+        <path d="M7 2C4.2 2 2 4.2 2 7v10c0 2.8 2.2 5 5 5h10c2.8 0 5-2.2 5-5V7c0-2.8-2.2-5-5-5H7zm5 5.5A4.5 4.5 0 1 1 7.5 12 4.5 4.5 0 0 1 12 7.5zm6-1.3a1.1 1.1 0 1 1-1.1-1.1A1.1 1.1 0 0 1 18 6.2z"/>
+      </svg>
+    </a>
+  </li>
+
+  <li>
+    <a href="https://facebook.com" target="_blank">
+      <!-- Facebook -->
+      <svg viewBox="0 0 24 24" width="22" height="22" fill="currentColor">
+        <path d="M22 12a10 10 0 1 0-11.6 9.9v-7H7.7V12h2.7V9.8c0-2.7 1.6-4.2 4-4.2 1.2 0 2.5.2 2.5.2v2.8h-1.4c-1.4 0-1.9.9-1.9 1.8V12h3.2l-.5 2.9h-2.7v7A10 10 0 0 0 22 12z"/>
+      </svg>
+    </a>
+  </li>
+
+  <li>
+    <a href="https://twitter.com" target="_blank">
+      <!-- X / Twitter -->
+      <svg viewBox="0 0 24 24" width="22" height="22" fill="currentColor">
+        <path d="M18.9 2H22l-7.5 8.6L23 22h-6.9l-5.4-6.9L4.3 22H1.2l8-9.2L1 2h7l4.9 6.2L18.9 2z"/>
+      </svg>
+    </a>
+  </li>
+</ul>
     </div>
   </div>
 </footer>
