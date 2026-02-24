b62876da50be40eb3c9260abff5e007929f057af
ilaaaaaaaa <162697755+ilaaaaaaaa@users.noreply.github.com>
2026-02-24 13:03:49 +0100

    Pagina contatti.php

--- pages/contatti.php
+++ pages/contatti.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nexus</title>
     <link rel="stylesheet" href="/Nexus-Space/assets/css/base.css">
-    <link rel="stylesheet" href="/Nexus-Space/assets/css/style.css">
+    <link rel="stylesheet" href="/Nexus-Space/assets/css/contatti.css">
 
     <!-- Google Font elegante -->
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@500;700&family=Inter:wght@300;400;500&display=swap" rel="stylesheet">
@@ -15,6 +15,26 @@
 <!-- HEADER -->
 <?php include __DIR__ . '/../includes/header.php'; ?>
 
+<section class="hero">
+    <div class="hero-text">
+        <h1>Contatti</h1>
+        <h4>Grazie per aver visitato il nostro sito!</h4>
+        <p>Per maggiori informazioni, contatta i nostri fondatori.</p>
+    </div>
+
+    <div class="founders">
+        <div class="founder-card">
+            <img src="/Nexus-Space/assets/img/Ilaria_Fragola.jpg" alt="Ilaria Fragola">
+            <h3>Ilaria Meggiolaro</h3>
+            <p><a href="mailto:ilaria.meggiolaro@nexus-space.com">ilaria.meggiolaro@nexus-space.com</a></p>
+        </div>
+        <div class="founder-card">
+            <img src="/Nexus-Space/assets/img/Shuyi_Mirtillo.jpg" alt="Shuyi Mirtillo">
+            <h3>Shuyi Wang</h3>
+            <p><a href="mailto:shuyi.wang@nexus-space.com">shuyi.wang@nexus-space.com</a></p>
+        </div>
+    </div>
+</section>
 
 <!-- FOOTER -->
 <?php include __DIR__ . '/../includes/footer.php'; ?>
